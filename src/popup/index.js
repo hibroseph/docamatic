f2f4338b29517ac5f20025cd3e25756634655231
hibroseph <iwillridgley@gmail.com>
2018-11-30 01:58:22 -0700

    create node to mount to for popup

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import Popup from './popup'
+import React from "react";
+import Popup from "./popup";
 
-let root = document.getElementById("__POPUP__MOUNT__POINT__")
+let root = document.getElementById("__POPUP__MOUNT__POINT__");
 
 if (!root) {
-    console.log("There is no root mount point for us :(")
+  console.log("There is no root mount point for us :(");
+  root = document.createElement("div");
+
+  document.body.appendChild(root);
 } else {
-    console.log("Yay, I found a mount point :)")
+  console.log("Yay, I found a mount point :)");
 }
 
 ReactDOM.render(
-    <Popup></Popup>
-)
+  <Provider>
+    <Popup />
+  </Provider>,
+  root
+);

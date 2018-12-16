2f14310b75bcce43e6f6b0341c3ab5bd3aa1d557
hibroseph <iwillridgley@gmail.com>
2018-12-16 00:54:03 -0700

    added note colors to search results

--- src/components/MiniSearchNote.js
+++ src/components/MiniSearchNote.js
@@ -3,15 +3,15 @@ import { MiniSearchNoteContainer } from "../elements/MiniSearchNoteContainer";
 
 class MiniSearchNote extends Component {
   render() {
-    console.log("MiniSearchNote body " + this.props.body);
-    console.log("MiniSearchNote title " + this.props.title);
+    // console.log("MiniSearchNote body " + this.props.body);
+    // console.log("MiniSearchNote title " + this.props.title);
 
-    console.log("this.props.searchQuery:" + this.props.searchQuery);
-    console.log("this.props.text[0]:" + this.props.text[0]);
-    console.log("this.props.text[1]:" + this.props.text[1]);
+    // console.log("this.props.searchQuery:" + this.props.searchQuery);
+    // console.log("this.props.text[0]:" + this.props.text[0]);
+    // console.log("this.props.text[1]:" + this.props.text[1]);
 
-    console.log("this.props.text:");
-    console.log(this.props.text);
+    // console.log("this.props.text:");
+    // console.log(this.props.text);
 
     let boldedSearchQuery = [];
 
@@ -21,18 +21,21 @@ class MiniSearchNote extends Component {
       console.log("<span> [i]" + this.props.text[i]);
       boldedSearchQuery.push(<span>{this.props.text[i]}</span>);
       console.log("<span>" + this.props.searchQuery);
-      if (i < (this.props.text.length - 1)) {
+
+      //   Ledt's make sure the last search Query doesn't appear
+      if (i < this.props.text.length - 1) {
         boldedSearchQuery.push(<b>{this.props.searchQuery}</b>);
       }
-      //   console.log("<span> [i+1]" + this.props.text[i+1])
-      //   boldedSearchQuery.push(<span>{this.props.text[i + 1]}</span>);
     }
 
-    console.log("boldedSearchQuery");
-    console.log(boldedSearchQuery);
+    // console.log("boldedSearchQuery");
+    // console.log(boldedSearchQuery);
 
     return (
-      <MiniSearchNoteContainer>
+      <MiniSearchNoteContainer
+      style={{
+          backgroundColor: this.props.color
+      }}>
         <div className="title_bar">
           <div> {this.props.title} </div>
         </div>

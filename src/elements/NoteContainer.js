0af1b4d90d128f3ec247373dfa952f6840fe6393
hibroseph <iwillridgley@gmail.com>
2019-05-31 00:58:01 -0600

    cleaned up white border

--- src/elements/NoteContainer.js
+++ src/elements/NoteContainer.js
@@ -13,13 +13,14 @@ export const NoteContainer = styled.div`
 
   .note {
     position: absolute;
-    /* top: 300px; */
-    /* left: 300px; */
     width: 100%;
     height: 100%;
 
     background-color: #f4f4f4;
-    border-radius: 5px, 5px,0px, 0px;
+    
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+
     resize: both;
     overflow: auto;
 
@@ -57,8 +58,6 @@ export const NoteContainer = styled.div`
     height: 60px;
 
     background-color: #35a1ec;
-    /* border-top-left-radius: 5px; */
-    /* border-top-right-radius: 5px; */
   }
 
   .title-input::placeholder {
